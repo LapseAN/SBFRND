3ca935ef73f43d41a45a9c8a5593f349e4eddbca
Afeef Nawar <an@lapse.com>
2022-04-03 12:09:39 -0700

    Debug

--- Screens/SigninScreen/SigninScreen.js
+++ Screens/SigninScreen/SigninScreen.js
@@ -2,10 +2,12 @@ import React from "react";
 import { View, Text } from "react-native";
 
 const SigninScreen = () => {
-  return;
-  <View>
-    <Text>Sign-in Screen</Text>
-  </View>;
+return
+    (
+        <View>
+        <Text>Sign-in Screen</Text>
+        </View>
+    )
 };
 
 export default SigninScreen;
